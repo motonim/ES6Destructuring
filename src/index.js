0a60d1ac320d1bc80bf80f5adb9a292969e6114c
Jaeri Park <59538964+motonim@users.noreply.github.com>
2021-02-19 16:09:12 -0500

    myanswer

--- src/index.js
+++ src/index.js
@@ -1,33 +1,43 @@
-import animals from "./data";
-
-console.log(animals);
-
-const [cat, dog] = animals;
-console.log(cat);
-
-const { name, sound } = cat;
-console.log(sound);
-
 // CHALLENGE: uncomment the code below and see the car stats rendered
-// import React from "react";
-// import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import cars from "./practice";
 
-// ReactDOM.render(
-//   <table>
-//     <tr>
-//       <th>Brand</th>
-//       <th>Top Speed</th>
-//     </tr>
-//     <tr>
-//       <td>{tesla.model}</td>
-//       <td>{teslaTopSpeed}</td>
-//       <td>{teslaTopColour}</td>
-//     </tr>
-//     <tr>
-//       <td>{honda.model}</td>
-//       <td>{hondaTopSpeed}</td>
-//       <td>{hondaTopColour}</td>
-//     </tr>
-//   </table>,
-//   document.getElementById("root")
-// );
+const [honda, tesla] = cars;
+const {
+  model: hondaName,
+  coloursByPopularity: hondaColour,
+  speedStats: hondaSpeed
+} = honda;
+
+const {
+  model: teslaName,
+  coloursByPopularity: teslaColour,
+  speedStats: teslaSpeed
+} = tesla;
+
+const { topSpeed: teslaTopSpeed, zeroTosixty: teslaZeroToSixty } = teslaSpeed;
+const teslaTopColour = teslaColour[0];
+
+const { topSpeed: hondaTopSpeed, zeroToSixty: hondaZeroToSixty } = hondaSpeed;
+const hondaTopColour = hondaColour[0];
+
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
+);
